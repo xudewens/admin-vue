756ef8cf5ac6e3a6ce1d80d40aff89745355d3de
amot <amot@trustineer.com>
2024-11-25 18:23:14 +0800

    首页底部信息

--- src/views/bottomInfo.vue
+++ src/views/bottomInfo.vue
@@ -5,8 +5,34 @@
             <div class="content">
                 GLOBGAME is one of the outstanding e-commerce platforms in Indonesia. We are committed to building a comprehensive e-commerce service platform that integrates product display, online transactions, payment settlement, and logistics distribution. Since its e
             </div>
+            <div class="footer-suggestioN">
+                <img src="../assets/images/C.png" alt="" width="14px">  2024 GLOBGAME. All Rights Reserved
+            </div>
+            <div class="websiteTerms">
+                <span>Website Terms and Conditions</span>
+                <span>User Terms & Conditions</span>
+                <span>Privacy Policy</span>
+            </div>
         </div>
-        <div class="right"></div>
+        <div class="right">
+            <p class="top" style="margin-bottom: 5px;">PT CAHAYA LENTERA HARMONI</p>
+            <p class="midText">Reach Us At</p>
+            <p class="smallText" style="margin-bottom: 10px; display: flex; align-items: center;">
+                <svg t="1732529754487" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2926" width="16" height="16"><path d="M838.954667 234.666667H170.666667c-3.626667 0-7.168 0.448-10.56 1.322666l323.690666 323.669334a21.333333 21.333333 0 0 0 30.165334 0L838.954667 234.666667z m46.144 14.186666l-260.693334 260.693334 262.933334 262.912c5.44-7.168 8.661333-16.106667 8.661333-25.792V277.333333c0-10.944-4.117333-20.906667-10.88-28.48zM843.861333 789.333333l-249.6-249.621333-50.133333 50.133333a64 64 0 0 1-90.517333 0l-50.112-50.133333L156.373333 786.88c4.48 1.578667 9.28 2.453333 14.314667 2.453333h673.194667zM128.661333 754.218667L373.333333 509.525333 129.578667 265.813333A42.709333 42.709333 0 0 0 128 277.333333v469.333334c0 2.56 0.213333 5.098667 0.661333 7.552zM170.666667 192h682.666666a85.333333 85.333333 0 0 1 85.333334 85.333333v469.333334a85.333333 85.333333 0 0 1-85.333334 85.333333H170.666667a85.333333 85.333333 0 0 1-85.333334-85.333333V277.333333a85.333333 85.333333 0 0 1 85.333334-85.333333z" fill="#FFFFFF" p-id="2927"></path>
+                </svg>
+                <span style="margin-left: 5px;">
+                    globalmitraberjaya2024@gmail.com
+                </span>
+            </p>
+            <p class="smallText">Monday to Friday：09.00-18.00</p>
+            <p class="smallText" style="margin-bottom: 10px;">Saturday and Sunday：10.00-17.00 </p>
+            <p class="midText">Direktorat Jenderal Perlindungan Konsumen dan Tertib Niaga</p>
+            <p class="midText">Kementerian Perdagangan Republik Indonesia </p>
+            <p class="midText" style="display: flex; align-items: center;">
+                <svg t="1732530064273" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5334" width="16" height="16"><path d="M714.666667 595.626667C725.333333 601.173333 732.16 604.16 734.293333 608.426667 736.853333 613.12 736 634.453333 725.333333 658.773333 716.8 682.666667 672.426667 705.706667 652.8 706.56 633.173333 707.413333 632.746667 721.92 526.506667 675.413333 420.266667 628.906667 356.266667 515.413333 351.146667 508.16 346.026667 500.906667 310.186667 449.28 311.893333 396.8 314.026667 344.746667 341.333333 320 352.426667 309.76 362.666667 298.666667 374.186667 297.386667 381.44 298.666667L401.493333 298.666667C407.893333 298.666667 416.853333 296.106667 424.96 317.866667L454.4 397.653333C456.96 403.2 458.666667 409.6 454.826667 416.426667L443.306667 433.92 426.666667 451.84C421.546667 456.96 415.573333 462.506667 421.546667 473.173333 426.666667 484.266667 448 519.68 477.866667 549.12 516.693333 586.666667 550.826667 599.04 561.066667 604.586667 571.306667 610.56 577.706667 609.706667 584.106667 602.88L618.666667 562.773333C626.773333 552.106667 633.6 554.666667 643.413333 558.08L714.666667 595.626667M512 85.333333C747.52 85.333333 938.666667 276.48 938.666667 512 938.666667 747.52 747.52 938.666667 512 938.666667 427.946667 938.666667 349.866667 914.346667 283.733333 872.533333L85.333333 938.666667 151.466667 740.266667C109.653333 674.133333 85.333333 596.053333 85.333333 512 85.333333 276.48 276.48 85.333333 512 85.333333M512 170.666667C323.413333 170.666667 170.666667 323.413333 170.666667 512 170.666667 585.386667 193.706667 653.226667 232.96 708.693333L192 832 315.306667 791.04C370.773333 830.293333 438.613333 853.333333 512 853.333333 700.586667 853.333333 853.333333 700.586667 853.333333 512 853.333333 323.413333 700.586667 170.666667 512 170.666667Z" p-id="5335" fill="#44ff00"></path></svg>
+                <span style="margin-left: 5px;"> WhatsApp:  0853-1111-1010</span>
+            </p>
+        </div>
     </div>
 </template>
 
@@ -33,7 +59,7 @@ export default {
         #bottom {
             display: flex;
             .left {
-                width: 70%;
+                width: 65%;
                 .logo {
                     width: 410px;
                     height: 50px;
@@ -41,7 +67,32 @@ export default {
                     margin-bottom: 20px;
                 }
                 .content {
-                    font-size: 18px;
+                    font-size: 14px;
+                    margin-bottom: 40px;
+                }
+                .footer-suggestioN {
+                    display: flex;
+                    align-items: center;
+                    margin-bottom: 5px;
+                    img {
+                        margin-right: 4px;
+                    }
+                }
+                .websiteTerms {
+                    font-size: 12px;
+                    color: #f97300;
+                    span {
+                        margin-right: 20px;
+                        cursor: pointer;
+                    }
+                }
+            }
+            .right {
+                width: 35%;
+                padding-left: 5px;
+                .top {
+                    font-size: 20px;
+                    font-weight: 600;
                 }
             }
         }
